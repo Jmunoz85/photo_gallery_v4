db73506d388f3ff589af3fd72553f1709c463a0e
Jose Munoz <joseangel091785@gmail.com>
2019-08-27 22:21:58 -0500

    final

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const searchBar = $('#search').querySelector('input');
-searchBar.addEventListener(keyup, function (event) {
+const searchBar = document.querySelector('input');
+searchBar.addEventListener("keyup", function (event) {
   const term = event.target.value.toLowerCase();
-  const photos = list.getElementsByClassName('gallery');
+  const gallery = document.getElementsByClassName("gallery")[0]
+  const photos = gallery.getElementsByTagName('a');
   Array.from(photos).forEach(function (photo) {
-    const title = photo.firstElementChild.text.content;
-    if (title.toUpperCase().indexOf(term) != -1) {
+    const title = photo.getAttribute('data-title');
+    console.log(title)
+    if (title.toLowerCase().indexOf(term) != -1) {
       photo.style.display = 'block';
     } else {
       photo.style.display = 'none';
